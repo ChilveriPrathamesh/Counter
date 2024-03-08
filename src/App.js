649c56870623ba58bde07c56935c26c43bea65d1
ChilveriPrathamesh <chilveriprathamesh@gmail.com>
2024-03-08 03:51:20 +0000

    Counter

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import Welcome from './components/Welcome'
+
+// const App = () => {
+//   return <Welcome name="Rahul" />
+// }
+// export default App
+
+import Counter from './components/Counter'
+const App = () => <Counter />
+
+export default App
